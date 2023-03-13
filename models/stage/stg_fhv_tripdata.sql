3d540242df3ea14fdc73090ca94aeda32f978456
AQsAKo <>
2023-03-13 19:09:00 +0300

    fix postgres profile

--- models/stage/stg_fhv_tripdata.sql
+++ models/stage/stg_fhv_tripdata.sql
@@ -1,5 +1,12 @@
+{{ config(materialized='view') }}
 
 with fhv_data as (
     select * from {{ source('stage','fhv') }}
 )
 select * from fhv_data
+
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
